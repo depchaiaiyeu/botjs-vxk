315a358fe77c588ef2d3424e32faf1530628052d
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-10-17 16:32:14 +0700

    Update logReplyData.js

--- src/service-hahuyhoang/tien-ich/logReplyData.js
+++ src/service-hahuyhoang/tien-ich/logReplyData.js
@@ -1,20 +1,27 @@
-import { sendMessageCompleteRequest, sendMessageFailed } from "../../service-hahuyhoang/chat-zalo/chat-style/chat-style.js";
-export async function logReply(api, message) {
+import { sendMessageFromSQL, sendMessageFailed, sendMessageQuery } from "../../service-hahuyhoang/chat-zalo/chat-style/chat-style.js";
+
+export async function handleGetMessageCommand(api, message) {
   try {
     const quote = message.data?.quote || message.reply;
-
     if (!quote) {
-      await sendMessageFailed(api, message, "Không có dữ liệu REPLY hoặc chưa reply tin nhắn cần lấy dữ liệu");
+      await sendMessageQuery(api, message, "Reply tin nhắn cần lấy thông tin! 🤔");
       return;
     }
     if (quote.attach) {
       quote.attach = JSON.parse(quote.attach);
       if (quote.attach.params) {
-        quote.attach.params = JSON.parse(quote.attach.params.replace(/\\\\/g, '\\').replace(/\\\//g, '/'));
+        quote.attach.params = JSON.parse(quote.attach.params.replace(/\\\\/g, "\\").replace(/\\\//g, "/"));
       }
     }
-    const logMessage = `📡 Dữ liệu tin nhắn được reply:\n\n${JSON.stringify(quote, null, 2)}`;
-    await sendMessageCompleteRequest(api, message, { caption: logMessage }, 1800000);
+    const fromD = quote.fromD;
+    const ownerId = quote.ownerId;
+    const cliMsgId = quote.cliMsgId;
+    const cliMsgType = quote.cliMsgType;
+    const ttl = quote.ttl;
+    const msg = quote.msg || "Không có";
+    const attach = quote.attach && Object.keys(quote.attach).length > 0 ? JSON.stringify(quote.attach, null, 2) : "Không có đính kèm";
+    const logMessage = `Người gửi: ${fromD}\nID Người Gửi: ${ownerId}\ncliMsgId: ${cliMsgId}\ncliMsgType: ${cliMsgType}\nTime to live: ${ttl}\nMsg: ${msg}\nĐính kèm: ${attach}`;
+    await sendMessageFromSQL(api, message, { caption: logMessage }, 1800000);
   } catch (error) {
     const errorMessage = `Đã xảy ra lỗi khi gửi log dữ liệu: ${error.message}`;
     await sendMessageFailed(api, message, errorMessage);
