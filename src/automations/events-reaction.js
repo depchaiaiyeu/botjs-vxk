f03b2c50550e95e8ed9098a914d2183dee6393c5
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-10-20 16:50:50 +0700

    Update events-reaction.js

--- src/automations/events-reaction.js
+++ src/automations/events-reaction.js
@@ -1,6 +1,6 @@
 import { handleReactionConfirmJoinGroup } from "../commands/bot-manager/remote-action-group.js";
 import { handleTikTokReaction } from "../service-hahuyhoang/api-crawl/tiktok/tiktok-service.js";
-
+import { handleAdminReactionDelete } from "../commands/bot-manager/recent-message.js";
 export async function reactionEvents(api, reaction) {
   if (await handleReactionConfirmJoinGroup(api, reaction)) return;
   const message = {
